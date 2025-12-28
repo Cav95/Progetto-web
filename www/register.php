9baca6cdfe51fbff60eb57f507c44cb6e384b7ec
Matteo Grandini <matteo.grandini@studio.unibo.it>
2025-12-28 12:30:35 +0100

    Removed old code from register.php

--- www/register.php
+++ www/register.php
@@ -1,25 +1,6 @@
 <?php
 require_once "bootstrap.php";
 
-if (isset($_POST["username"]) && isset($_POST["password"]) && isset($_POST["password_R"])) {
-  $username = $_POST["username"];
-  $password = $_POST["password"];
-  $password_R = $_POST["password_R"];
-
-  if ($password != $password_R) {
-    $templateParams["errmsg"] = "Le password inserite non corrispondono!";
-  } else {
-    if ($dbh->doesUserExist($username)) {
-      $templateParams["errmsg"] = "Questo username esiste già!";
-    } else {
-      $hash = password_hash($password, PASSWORD_ARGON2ID);
-      $dbh->registerUser($username, $hash);
-      $templateParams["infomsg"] = "Registrazione avvenuta con successo!";
-    }
-  }
-
-}
-
 // Base
 $templateParams["curiosita"] = $dbh->getRandomFacts(3);
 
