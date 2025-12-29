f8ab64e554d2aa51349bdf5a4cc93c68383b9085
Mattia Cavina <mattiacavina95@gmail.com>
2025-12-29 20:07:28 +0100

    Create index home page

--- www/template/home.php
+++ www/template/home.php
@@ -1,5 +1,55 @@
+<head>
+  <style>
+    #cane {
+      width: 90%;
+    }
+
+    @media screen and (min-width:768px) {
+      #cane {
+        width: 30%;
+      }
+    }
+  </style>
+</head>
 <h2 class="text-center my-3 h1">Home</h2>
-<div class="py-3">
-  <img src="<?php echo PET_IMG_DIR."varietaAnimali.jpg" ?>" alt="Animali da compagnia" class="w-100 border border-2">
+<div class="col-1"></div>
+<div class="col-md-10 offset-md-1 text-center">
+  <h3>CHI SIAMO</h3>
+  <p>La nostra associazione promuove servizi volti al miglioramento del benessere psicofisico della
+    persona attraverso la relazione con gli animali a fini educativi e co-terapeutici, tramite Interventi Assistiti con
+    Animali.</p>
+  <p>In particolare ci preme aiutare gli studenti perchè la correlazione positiva si spiega con il meccanismo del
+    conforto non giudicante. A differenza di professori,
+    genitori o compagni di corso, l'animale non valuta la performance accademica dello studente. Questo offre una "pausa
+    psicologica" totale dalle aspettative di rendimento, permettendo al cervello di ricaricarsi.
+    Non serve necessariamente possedere un animale. I benefici massimi si riscontrano spesso nei
+    programmi organizzati dagli atenei (es. Puppy Rooms o giornate anti-stress), dove l'interazione è guidata e non
+    comporta per lo studente l'onere della gestione dell'animale.</p>
+  <img src="<?php echo PET_IMG_DIR . "varietaAnimali.jpg" ?>" alt="Animali da compagnia" class="w-100 border border-2">
+</div>
+<div id="benefici" class="p-5">
+  <div class="offset-md-1 float-start">
+    <h3 class="text-center">Perchè il cane?</h3>
+    <h4>Perchè stimola:</h4>
+    <ul>
+      <li>Fantasia</li>
+      <li>Curiosità</li>
+      <li>Capacità di osservazione</li>
+      <li>Interazione</li>
+    </ul>
+    <h4>Il cane inoltre:</h4>
+    <ul>
+      <li>Accetta incondizionatamente chiunque (accettazione della diversità)</li>
+      <li>È disinteressato</li>
+      <li>Dà incondizionatamente affetto</li>
+      <li>Dà senso di protezione</li>
+      <li>Suscita sentimenti di protezione</li>
+      <li>Dà una stimolazione plurisensoriale</li>
+      <li>Utilizza più “linguaggi” e canali di comunicazione</li>
+      <li> Utilizza messaggi facilmente comprensibili</li>
+    </ul>
+  </div>
+  <img id="cane" src="<?php echo PET_IMG_DIR . "placeholder-dog.jpg" ?>" alt="Padrone che abbraccia il suo cane"
+    class="img-fluid rounded float-end">
 
 </div>
