4c4a9c10a8c87ff9c41bef03577f2052380978e5
Matteo Grandini <matteo.grandini@studio.unibo.it>
2025-12-29 12:34:25 +0100

    Index placeholder

--- www/template/intro.php
+++ www/template/intro.php
@@ -1,3 +1,6 @@
-<!-- TODO -->
-
-<h2 class="h1 text-center mt-3">Index</h2> <!-- Placheholder -->
+<!-- Placheholder -->
+<h2 class="text-center my-3 h1">Index</h2>
+<div class="py-3">
+  <img src="<?php echo PET_IMG_DIR."varietaAnimali.jpg" ?>" alt="Animali da compagnia" class="w-100 border border-2">
+</div>
+<!-- Placheholder -->
