3a38817177b0d4d31830d2d27e9fb6c8b12a24b7
Matteo Grandini <matteo.grandini@studio.unibo.it>
2025-12-31 18:46:27 +0100

    More design improv.

--- www/template/admin-home.php
+++ www/template/admin-home.php
@@ -36,7 +36,7 @@
   <div class="col col-md-10 vstack mx-md-0 mx-1">
     <?php foreach ($templateParams["prenotazioni"] as $prenotazione): ?>
       <div class="card mb-3 px-2 bg-dark text-white" id="<?php echo "app-" . $prenotazione["id_prenotazione"]; ?>">
-        <div class="card-body row gap-md-2">
+        <div class="card-body row">
           <div class="col-md-2 d-flex align-items-center gap-2">
             <svg xmlns="http://www.w3.org/2000/svg" role="img" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16" aria-labelledby="ora-label">
               <title id="ora-label">Ora</title>
@@ -45,7 +45,7 @@
             </svg>
             <?php echo $prenotazione["ora"]; ?>
           </div>
-          <div class="col-md-2 d-flex align-items-center gap-2">
+          <div class="col-md-3 d-flex align-items-center gap-2">
             <svg xmlns="http://www.w3.org/2000/svg" role="img" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16" aria-labelledby="posizione-label">
               <title id="posizione-label">Posizione</title>
               <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
