1ba9b4c2370d5130975a09b1869cba33b7bbe741
Mattia Cavina <mattiacavina95@gmail.com>
2026-01-01 18:21:11 +0100

    Modify button

--- www/add-pet.php
+++ www/add-pet.php
@@ -10,14 +10,13 @@ $templateParams["title"] = "Add pet | Unibo Pet Therapy";
 $templateParams["nome"] = "pet-form.php";
 $templateParams["js"] = ["js/add-pet.js"];
 
-// Specific
-$templateParams["formaction"] = "Aggiungi";
-
 //Pet feuture
 $templateParams["specie"] = $dbh->getSpecie();
 $templateParams["razza"] = $dbh->getRace();
 
 if(isset($_REQUEST['pet-id'])){
+  $templateParams["formaction"] = "Modifica";
+
   $temp= $dbh->getSinglePet($_REQUEST['pet-id']);
   $templateParams["specificpet"]["Nomepet"] = $temp[0]["Nomepet"];
   $templateParams["specificpet"]["DataDiNascita"] = $temp[0]["DataDiNascita"];
@@ -26,10 +25,12 @@ if(isset($_REQUEST['pet-id'])){
   $templateParams["specificpet"]["Descrizione"] = $temp[0]["Descrizione"];
   $templateParams["specificpet"]["Immagine"] = $temp[0]["Immagine"];
   $templateParams["specificpet"]["DescrizioneImmagine"] = $temp[0]["DescrizioneImmagine"];
-  $templateParams["specificpet"]["Disponibile"] = $temp[0]["Disponibile"];
+  $templateParams["specificpet"]["Disponibile"] = $temp[0]["Disponibile"] == 1? "yes" : "no";
 
 }
 else{
+  $templateParams["formaction"] = "Aggiungi";
+
   $templateParams["specificpet"]["Nomepet"] = ""; 
   $templateParams["specificpet"]["DataDiNascita"] = ""; 
   $templateParams["specificpet"]["Nomespecie"] = "";    
--- www/template/pet-form.php
+++ www/template/pet-form.php
@@ -7,58 +7,67 @@
         <div class="alert alert-success d-none" id="alert-success"></div>
         <form action="#" method="POST">
           <div class="form-floating mb-3">
-            <input type="text" name="nome" class="form-control" id="nome" placeholder="Nome" autocomplete="given-name" value="<?php echo $templateParams["specificpet"]["Nomepet"];?>" required>
+            <input type="text" name="nome" class="form-control" id="nome" placeholder="Nome" autocomplete="given-name"
+              value="<?php echo $templateParams["specificpet"]["Nomepet"]; ?>" required>
             <label for="nome" class="form-label">Nome</label>
           </div>
-              <div class="form-floating mb-3">
-                <input type="date" name="data" class="form-control" id="data" placeholder="Data" value="<?php echo $templateParams["specificpet"]["DataDiNascita"];?>" required>
-                <label for="data" class="form-label">Data</label>
-              </div>
+          <div class="form-floating mb-3">
+            <input type="date" name="data" class="form-control" id="data" placeholder="Data"
+              value="<?php echo $templateParams["specificpet"]["DataDiNascita"]; ?>" required>
+            <label for="data" class="form-label">Data</label>
+          </div>
           <div>
             <select id="spec-sel" class="form-select form-select-sm mb-3" aria-label="Small select example">
-              <option selected><?php echo $templateParams["specificpet"]["Nomespecie"];?></option>               
+              <option selected><?php echo $templateParams["specificpet"]["Nomespecie"]; ?></option>
               <?php foreach ($templateParams["specie"] as $specie):
-              ?>
-                <option value="<?php echo $specie["ID_Specie"]; ?>" id="nomespecie"><?php echo $specie["Nomespecie"]; ?></option>
+                ?>
+                <option value="<?php echo $specie["ID_Specie"]; ?>" id="nomespecie"><?php echo $specie["Nomespecie"]; ?>
+                </option>
               <?php endforeach; ?>
             </select>
           </div>
           <div>
             <select id="razza-sel" class="form-select form-select-sm mb-3" aria-label="Small select example">
-              <option selected><?php echo $templateParams["specificpet"]["Nomerazza"];?></option>
+              <option selected><?php echo $templateParams["specificpet"]["Nomerazza"]; ?></option>
               <?php foreach ($templateParams["razza"] as $razza):
-              ?>
-                <option value="<?php echo $razza["ID_Razza"]; ?>" id="nomerazza"><?php echo $razza["Nomespecie"] . "-" . $razza["Nomerazza"]; ?></option>
+                ?>
+                <option value="<?php echo $razza["ID_Razza"]; ?>" id="nomerazza">
+                  <?php echo $razza["Nomespecie"] . "-" . $razza["Nomerazza"]; ?></option>
               <?php endforeach; ?>
             </select>
           </div>
 
           <div class="form-floating mb-3">
-            <input type="text" name="descrizione" class="form-control" id="descrizione" placeholder="Descrizione" value="<?php echo $templateParams["specificpet"]["Descrizione"];?>"required>
+            <input type="text" name="descrizione" class="form-control" id="descrizione" placeholder="Descrizione"
+              value="<?php echo $templateParams["specificpet"]["Descrizione"]; ?>" required>
             <label for="descrizione" class="form-label">Descrizione</label>
           </div>
           <div class="mb-3">
             <label for="formFile" class="form-label">Scegli Immagine Animale</label>
-            <input class="form-control" type="file" id="pet-img" value="<?php echo $templateParams["specificpet"]["Immagine"];?>">
+            <input class="form-control" type="file" id="pet-img"
+              value="<?php echo $templateParams["specificpet"]["Immagine"]; ?>">
           </div>
-                    <div class="form-floating mb-3">
-            <input type="text" name="nome" class="form-control" id="descrizione-img" placeholder="Descrizione Immagine" value="<?php echo $templateParams["specificpet"]["DescrizioneImmagine"];?>"required>
+          <div class="form-floating mb-3">
+            <input type="text" name="nome" class="form-control" id="descrizione-img" placeholder="Descrizione Immagine"
+              value="<?php echo $templateParams["specificpet"]["DescrizioneImmagine"]; ?>" required>
             <label for="descrizione-img" class="form-label">Descrizione Immagine</label>
           </div>
           <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="<?php echo $templateParams["specificpet"]["Disponibile"];?>" id="disponibile">
+            <input class="form-check-input" type="checkbox"
+              value="<?php echo $templateParams["specificpet"]["Disponibile"]; ?>" id="disponibile">
             <label class="form-check-label" for="checkDefault">
               Disponibile
             </label>
             <div>
-          </div class="col-auto">
+            </div class="col-auto">
 
-          <div class="d-flex justify-content-start col">
-            <input type="submit" class="btn btn-primary my-3 px-3" value="<?php echo $templateParams["formaction"]; ?>">
-          </div>
-                      <div class="d-flex justify-content-end col">
-            <input type="submit" class="btn btn-primary my-3 px-3 bg-warning" value="Elimina">
-          </div>
+            <div class="d-flex justify-content-start col">
+              <input type="submit" class="btn btn-primary my-3 px-3"
+                value="<?php echo $templateParams["formaction"]; ?>">
+            </div>
+            <div class="d-flex justify-content-end col">
+              <input type="submit" class="btn btn-primary my-3 px-3 bg-warning" value="Elimina">
+            </div>
           </div>
         </form>
       </div>
