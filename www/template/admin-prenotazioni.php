c282cadfdefef7a7df1a8cc19aaa5d3cb9d775de
Matteo Grandini <matteo.grandini@studio.unibo.it>
2026-01-02 17:32:24 +0100

    Improved HTML5 syntax

--- www/template/admin-prenotazioni.php
+++ www/template/admin-prenotazioni.php
@@ -1,64 +1,66 @@
-<h2 class="text-center my-4 h2">Gestisci prenotazioni</h2>
-<div class="row row-cols-md-3 mb-5 gx-5 px-2 px-md-0 gy-3 align-items-center">
-  <div class="col-12 col-md order-first order-md-2">
-    <form action="#" method="POST">
-      <div class="form-floating">
-        <input type="date" name="data" class="form-control" id="data" placeholder="Data" value="<?php echo date("Y-m-d"); ?>" required>
-        <label for="data" class="form-label">Data</label>
+<section>
+  <h2 class="text-center my-4 h2">Gestisci prenotazioni</h2>
+  <div class="row row-cols-md-3 mb-5 gx-5 px-2 px-md-0 gy-3 align-items-center">
+    <div class="col-12 col-md order-first order-md-2">
+      <form action="#" method="POST">
+        <div class="form-floating">
+          <input type="date" name="data" class="form-control" id="data" placeholder="Data" value="<?php echo date("Y-m-d"); ?>" required>
+          <label for="data" class="form-label">Data</label>
+        </div>
+        <input type="submit" hidden>
+      </form>
+    </div>
+    <div class="col-6 col-md order-2 order-md-first">
+      <div class="d-flex justify-content-end">
+        <button class="btn btn-primary d-flex align-items-center gap-2" id="date-bwd">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
+          </svg>
+          Indietro
+        </button>
       </div>
-      <input type="submit" hidden>
-    </form>
-  </div>
-  <div class="col-6 col-md order-2 order-md-first">
-    <div class="d-flex justify-content-end">
-      <button class="btn btn-primary d-flex align-items-center gap-2" id="date-bwd">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
-        </svg>
-        Indietro
-      </button>
+    </div>
+    <div class="col-6 col-md order-last">
+      <div class="d-flex justify-content-start">
+        <button class="btn btn-primary d-flex align-items-center gap-2" id="date-fwd">
+          Avanti
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
+          </svg>
+        </button>
+      </div>
     </div>
   </div>
-  <div class="col-6 col-md order-last">
-    <div class="d-flex justify-content-start">
-      <button class="btn btn-primary d-flex align-items-center gap-2" id="date-fwd">
-        Avanti
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
-        </svg>
-      </button>
+  <div class="row">
+    <div class="col-md-1"></div>
+    <div class="col col-md-10 mx-md-0 mx-1">
+      <div class="h3 text-center mt-3 mx-2 alert alert-info d-none" id="no-app-alert">
+          Nessuna prenotazione per la data selezionata
+      </div>
+      <table class="table table-dark table-striped table-mobile-stack align-middle" id="app-table">
+        <thead>
+          <tr>
+            <th scope="col" id="ora">Ora</th>
+            <th scope="col" id="luogo">Luogo</th>
+            <th scope="col" id="utente">Utente</th>
+            <th scope="col" id="elimina"><span class="visually-hidden">Pulsanti elimina appuntamento</span></th>
+          </tr>
+        </thead>
+        <tbody id="app-container">
+          <!-- Appointments added with AJAX -->
+        </tbody>
+      </table>
     </div>
+    <div class="col-md-1"></div>
   </div>
-</div>
-<div class="row">
-  <div class="col-md-1"></div>
-  <div class="col col-md-10 mx-md-0 mx-1">
-    <div class="h3 text-center mt-3 mx-2 alert alert-info d-none" id="no-app-alert">
-        Nessuna prenotazione per la data selezionata
-    </div>
-    <table class="table table-dark table-striped table-mobile-stack align-middle" id="app-table">
-      <thead>
-        <tr>
-          <th scope="col" id="ora">Ora</th>
-          <th scope="col" id="luogo">Luogo</th>
-          <th scope="col" id="utente">Utente</th>
-          <th scope="col" id="elimina"><span class="visually-hidden">Pulsanti elimina appuntamento</span></th>
-        </tr>
-      </thead>
-      <tbody id="app-container">
-        <!-- Appointments added with AJAX -->
-      </tbody>
-    </table>
-  </div>
-  <div class="col-md-1"></div>
-</div>
+</section>
 
 <!-- Bootstrap Modal -->
 <div class="modal fade" id="confirmModal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="modalLabel">Conferma cancellazione</h1>
+        <h4 class="modal-title fs-5" id="modalLabel">Conferma cancellazione</h4>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
--- www/template/base.php
+++ www/template/base.php
@@ -10,7 +10,7 @@
   if(isset($templateParams["css"])):
     foreach($templateParams["css"] as $sheet):
   ?>
-    <link rel="stylesheet" href="<?php echo $sheet; ?>">
+      <link rel="stylesheet" href="<?php echo $sheet; ?>">
   <?php
     endforeach;
   endif;
@@ -21,17 +21,13 @@
 <body class="container-fluid bg-body p-0 overflow-x-hidden">
   <header class="bg-warning p-4">
     <?php require "navbar.php"; ?>
-
     <div class="row">
       <div class="col col-md-4">
         <h1 class="visually-hidden">Unibo Pet Therapy</h1>
         <a href="index.php"><img src="<?php echo DESIGN_IMG_DIR . "Titolo.png"; ?>" alt="Unibo Pet Therapy" class="img-fluid w-100"></a>
       </div>
-      <div class="col-md-8">
-
-      </div>
+      <div class="col-md-8"></div>
     </div>
-
   </header>
   <div class="row g-0 bg-body-tertiary">
     <div class="col-lg-9 col pb-4">
@@ -47,16 +43,14 @@
       <aside>
         <h3 class="text-center my-3 h4">Curiosità</h3>
         <div class="row px-2 g-3 pb-4">
-
           <?php foreach ($templateParams["curiosita"] as $curiosita): ?>
-          <div class="card col-12">
-            <div class="card-body">
-              <h5 class="card-title"><?php echo $curiosita["titolo"]; ?></h5>
-              <p class="card-text"><?php echo $curiosita["descrizione"]; ?></p>
+            <div class="card col-12">
+              <div class="card-body">
+                <h5 class="card-title"><?php echo $curiosita["titolo"]; ?></h5>
+                <p class="card-text"><?php echo $curiosita["descrizione"]; ?></p>
+              </div>
             </div>
-          </div>
           <?php endforeach; ?>
-          
         </div>
       </aside>
     </div>
@@ -65,15 +59,12 @@
     &copy; Mattia Cavina, Matteo Grandini — Tutti i diritti riservati
   </footer>
   
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
-    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   <?php
   if(isset($templateParams["js"])):
     foreach($templateParams["js"] as $script):
   ?>
-    <script src="<?php echo $script; ?>"></script>
+      <script src="<?php echo $script; ?>"></script>
   <?php
     endforeach;
   endif;
