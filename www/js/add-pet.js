44d119b76e4e7c8214c1f25ef780389b6160c68b
Mattia Cavina <mattiacavina95@gmail.com>
2026-01-03 09:35:18 +0100

    Delete command in pet modify

--- www/js/add-pet.js
+++ www/js/add-pet.js
@@ -47,7 +47,7 @@ form.addEventListener("submit", e => {
         id_pet
       );
       
-      window.location.href = 'add-pet.php?pet-id=' + id_pet;
+     // window.location.href = 'add-pet.php?pet-id=' + id_pet;
     }
   }
 
